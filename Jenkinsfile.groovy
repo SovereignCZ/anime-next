e2e419eb7e47e8a9ac3f0327dc14b8b13a65e563
Sovereign <SovereignCZ@gmail.com>
2024-07-16 16:21:52 +0200

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,7 +32,7 @@ node {
                 // Zastaví a odstraní předchozí kontejner, pokud existuje
                 sh 'docker rm -f anime-next || true'
                 // Spustí nový kontejner
-                sh 'docker run -d --name anime-next -label com.sove.projekt=anime -l label com.sove.jazyk=next -p 3000:3000 --restart unless-stopped anime-next '
+                sh 'docker run -d --name anime-next -l com.sove.projekt=anime -l com.sove.jazyk=next -p 3000:3000 --restart unless-stopped anime-next'
             }
         }
 
